746e24cfd85b25650b56680fd21eebf840001b70
AkiteDonald <donaldakite27@gmail.com>
2022-12-06 18:53:56 +0000

    Add dynamic loading

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
 import './style.css';
-import { camelCase } from 'lodash';
+// import './style.css';
 
-console.log(camelCase('hello world how are you'));
+const Dynamic = document.querySelector('.todo-maker');
+const todos = [
+    {
+        description : "wake up from bed",
+        completed : false,
+        index: 0,
+    },
+    {
+        description : "take break fast",
+        completed : false,
+        index: 1,
+    },
+    {
+        description : "play apex legends",
+        completed : false,
+        index: 2,
+    },
+    {
+        description : "read my books",
+        completed : false,
+        index: 3,
+    }
+]
+
+todos.forEach((deed) => {
+    const content = `
+            <div class="list">
+                <div class="list-action">
+                    <input type="checkbox">
+                    <p>${deed.description}</p>
+                </div>
+                <div class="action-container">
+                    <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+            </div>
+            <hr>`;
+            Dynamic.innerHTML += content;
+})
