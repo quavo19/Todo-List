9929f6c33f95133206c684cb2b065caf78f4103f
AkiteDonald <donaldakite27@gmail.com>
2022-12-08 10:16:03 +0000

    Add function for adding and removing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
-let todosArr = [];
+
+ const storage =()=>{
+    const todosArr = JSON.parse(localStorage.getItem('deeds')) || [] ;
+    return todosArr;
+ }
+// console.log(todosArr)
+
 class todo {
     constructor(description, completed = false, index){
         this.description = description;
@@ -8,38 +14,80 @@ class todo {
     }
 }
 const pushtodo = () =>{
-   const index = todo.length;
+    const todosArr = storage();
+   const index = todosArr.length;
    const description = document.getElementById('input').value;
    const completed = false;
    const todoList = new todo(description,completed,index);
    if(description != ''){
     todosArr.push(todoList);
+    localStorage.setItem('deeds', JSON.stringify(todosArr));
    }
 }
 
 const Dynamic = document.querySelector('.todo-maker');
-console.log(todosArr)
+// console.log(todosArr)
 let todos2=() => {
+    const todosArr = storage();
     Dynamic.innerHTML = '';
-    for (let i = 0; i < todosArr.length; i ++){
+    for (let i = 0; i < todosArr.length; i += 1){
+        let isComplte = todosArr[i].completed == true ? "checked" : "";
         Dynamic.innerHTML += `
-        <div class="list">
+        <div class="list ${isComplte}">
                 <div class="list-action">
-                    <input type="checkbox">
+                    <input class ="check" id ="${todosArr[i].index}" ${isComplte} type="checkbox">
                     <p>${todosArr[i].description}</p>
                 </div>
                 <div class="action-container">
-                    <div class="icon"><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                    <div class="icon"><i id = "vertical" class="fa-solid fa-ellipsis-vertical"></i> <i id = "${todosArr[i].index}" class="fa-solid fa-trash remove"></i></div>
                 </div>
             </div>
-            <hr>`;
+            `;
+
     }
 }
 
+todos2();
+
+const checked = document.querySelectorAll('.check');
+checked.forEach((button) => {
+    const todosArr = storage();
+    button.addEventListener('click', (e) => {
+       const card = e.path.filter((el) => el.classList?.contains('list')).at(0);
+       if(button.checked){
+        card.classList.add("checked");
+        todosArr[button.id].completed = true;
+       } else {
+        card.classList.remove("checked")
+        todosArr[button.id].completed = false;
+       }
+       localStorage.setItem('deeds', JSON.stringify(todosArr));
+    });
+  });
+
 document.addEventListener('keyup', (e) => {
+    const todosArr = storage();
     if (e.key === 'Enter') {
-        pushtodo();
-        todos2()
+        localStorage.setItem('deeds', JSON.stringify(todosArr));
+         pushtodo();
+         todos2();
+         window.location.reload();
         console.log(Dynamic)
     }
+});
+
+const rmv = document.querySelectorAll('.remove');
+ rmv.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    let todosArr = storage();
+     const card2 = e.path.filter((el) => el.classList?.contains('list')).at(0);
+     card2.remove()
+     todosArr.splice(item.id, 1);
+     todosArr.forEach((todo) => {
+        todo.index = todosArr.indexOf(todo);
+     });
+     localStorage.setItem('deeds', JSON.stringify(todosArr));
+     todos2();
+     location.reload();
+      });
 });
