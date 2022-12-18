a700e790d9c43a61dfa1742eb4827537537ce79b
AkiteDonald <donaldakite27@gmail.com>
2022-12-18 17:58:34 +0000

    Make changes

--- src/clear-all.js
+++ src/clear-all.js
@@ -1,23 +1,21 @@
+// eslint-disable-next-line import/no-cycle
 import storage from './index.js';
-const clean = () =>{
-const clearButton = document.querySelector('.clean');
 
-const checked = document.querySelectorAll('.checked');
-
-clearButton.addEventListener('click', () => {
-    let todosArr = storage();
-    const done = todosArr.filter(todosArr => todosArr.completed);
-    const id =  done.forEach(element => {
-        console.log(element.index);
-        todosArr.splice(element.index, 1)
-        todosArr.forEach((todo) => {
-            todo.index = todosArr.indexOf(todo);
-         });
-        });
-        localStorage.setItem('deeds', JSON.stringify(todosArr));
-        window.location.reload();
+const clean = () => {
+  const clearButton = document.querySelector('.clean');
+  clearButton.addEventListener('click', () => {
+    const todosArr = storage();
+    const done = todosArr.filter((todosArr) => todosArr.completed);
+    const id = done.forEach((element) => {
+      todosArr.splice(element.index, 1);
+      todosArr.forEach((todo) => {
+        todo.index = todosArr.indexOf(todo);
+      });
+    });
+    localStorage.setItem('deeds', JSON.stringify(todosArr));
+    window.location.reload();
     return id;
-    })
- }
+  });
+};
 
-export { clean as default }
+export { clean as default };
